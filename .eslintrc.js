c9867d6359aa4001b1472c13d4e314549aa26f3e
uuizdashdw <chany920@gmail.com>
2024-12-16 17:36:14 +0900

    1. 프로젝트 세팅

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+	extends: ['react-app', 'plugin:prettier/recommended'],
+	plugins: ['prettier'],
+	rules: {
+		'prettier/prettier': [
+			'error',
+			{
+				singleQuote: true,
+				semi: true,
+				useTabs: true,
+				tabWidth: 2,
+				trailingComma: 'all',
+				printWidth: 80,
+				bracketSpacing: true,
+				arrowParens: 'avoid',
+			},
+		],
+	},
+};
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+const App = () => {
+	return (
+		<div>
+			<div>asdas</div>
+		</div>
+	);
+};
+
+export default App;
